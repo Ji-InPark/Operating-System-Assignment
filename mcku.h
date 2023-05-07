827f394aa6c921ea6e7d1489fa0ea7af3e2ba6f1
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-05-07 14:10:30 +0900

    clean up

--- mcku.h
+++ mcku.h
@@ -1,9 +1,9 @@
 #include <string.h>
 #include <stdbool.h>
 
-char* deleteNewLine(char *str);
+char *deleteNewLine(char *str);
 
-struct pcb{
+struct pcb {
     char pid;
     FILE *fd;
     char *pgtable;
@@ -12,7 +12,7 @@ struct pcb{
 };
 
 // pcb 배열
-struct pcb* pcbs;
+struct pcb *pcbs;
 // 프로세스 개수
 int processLength;
 // mcku.c에서 사용하는 변수 가져오기 위한 선언
@@ -20,35 +20,35 @@ extern struct pcb *current;
 extern char *ptbr;
 
 // pid 1씩 올려서 current에 넣는 방식
-void ku_scheduler(char pid){
+void ku_scheduler(char pid) {
     // 무한루프 막기위해서 count 변수 선언
     int count = 0;
 
     do {
         current = &pcbs[++pid % processLength];
         ptbr = current->pgtable;
-    }while(current->isExit && count++ < processLength);
+    } while (current->isExit && count++ < processLength);
 
     // 만약 모든 프로세스가 exit됐다면 프로그램 종료
-    if(count >= processLength){
+    if (count >= processLength) {
         printf("all process is exit\n");
         exit(0);
     }
 }
 
 
-void ku_pgfault_handler(char pid){
+void ku_pgfault_handler(char pid) {
 
 }
 
 
-void ku_proc_exit(char pid){
+void ku_proc_exit(char pid) {
     printf("proc exit called\n");
     pcbs[pid].isExit = true;
 }
 
 
-void ku_proc_init(int nprocs, char *flist){
+void ku_proc_init(int nprocs, char *flist) {
     // 프로세스 개수 기억
     processLength = nprocs;
     // 프로세스 파일 리스트 들어있는 파일 불러오기
@@ -58,9 +58,9 @@ void ku_proc_init(int nprocs, char *flist){
     // 파일 줄단위로 읽을 때 필요함 - 하지만 안씀
     size_t len = 0;
 
-    for(int i = 0; i < nprocs; i++) {
+    for (int i = 0; i < nprocs; i++) {
         // 프로세스 파일명 읽어오기
-        char* processFileName = NULL;
+        char *processFileName = NULL;
         getline(&processFileName, &len, fileList);
         // 개행문자 지워주는 과정
         processFileName = deleteNewLine(processFileName);
@@ -77,12 +77,12 @@ void ku_proc_init(int nprocs, char *flist){
 }
 
 // 개행문자가 들어있으면 지우고 리턴 안들어있다면 그냥 리턴
-char* deleteNewLine(char *str) {
-    if(str[strlen(str) - 1] != '\n') return str;
+char *deleteNewLine(char *str) {
+    if (str[strlen(str) - 1] != '\n') return str;
 
-    char *newStr = (char*)malloc(strlen(str) - 1);
+    char *newStr = (char *) malloc(strlen(str) - 1);
 
-    for(int i = 0; i < strlen(str) - 1; i++) newStr[i] = str[i];
+    for (int i = 0; i < strlen(str) - 1; i++) newStr[i] = str[i];
 
     return newStr;
 }
